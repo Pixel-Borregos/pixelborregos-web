2f8c50f63e180dccbb901ceb4f082d7f18bdff87
Bernardo <bernardo.giron99@gmail.com>
2022-11-28 20:50:59 -0600

    Call news and projects from DB

--- src/components/News/js/newsBody.js
+++ src/components/News/js/newsBody.js
@@ -1,23 +1,42 @@
 import React from "react";
 import {Col, Container,Row} from "react-bootstrap"
 
+
 import NewsPreview from "./newsPreview";
+import { ReadDocument } from "../../../js/db/dbOperations";
+
 class NewsBody extends React.Component{
-    render(){
-        let news = []
-        for(let i =0; i < 10; i++){
-            news.push(<Row key={i.toString()}>
-                <Col>
-                    <NewsPreview
-                        title="Pixel Borregos and 48toPlay"
-                        body = "Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"
-                    />
-                </Col>
-            </Row>)
+    constructor(props){
+        super(props);
+        this.state = {
         }
+    }
+
+    getNews =async () =>{
+        let newsInfo =  await ReadDocument("pixelBlog")
+            .then( (result) => {
+                    let news = []
+                    for(let i = 0; i < result.length;i++){
+                        news.push(<NewsPreview
+                                    title={result[i].title}
+                                    body={result[i].body}
+                        />);
+                    }
+                    return news;
+                } 
+            );
+        this.setState({news:newsInfo})
+    }
+
+    componentDidMount = () =>{
+       this.getNews();
+    }
+
+    render(){
+        
         return(
             <Container fluid>            
-                {news}
+                {this.state.news}
             </Container>
         )
     }
